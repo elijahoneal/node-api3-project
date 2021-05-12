0fe3b85547cc649652f674922eea422003eb4125
Elijah <elijahoneal@yahoo.com>
2021-05-11 23:19:11 -0400

    Middleware attempt

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,48 @@
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log( `[${new Date().toISOString()}] ${req.method} to ${req.url} ` )
+
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  
+
+  if(id) {
+    req.user = user
+    next();
+  } else {
+    res.status(404).json({ message: "user not found" })
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const name = req.body.name
+  if(name){
+    next()
+  } else {
+    res.status(404).json({message: "missing required name field"})
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const text = req.body.text
+
+  if(text){
+    next()
+  } else {
+    res.status(404).json({message: "missing required text field"})
+  }
 }
 
 // do not forget to expose these functions to other modules
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,17 +1,32 @@
 const express = require('express');
-
+const User = require('./users-model')
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
-
+const middleware = require('../middleware/middleware')
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+  .then( users => res.status(200).json(users) )
+  .catch( err  => {
+    console.log(err)
+    res.status(500).json({ message: 'Something done broke' })
+  })
 });
 
 router.get('/:id', (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  const { id } = req.params
+ 
+  User.getById(id)
+  .then( user => {
+    middleware.validateUserId() } )
+  .catch( err=> {
+    console.log(err)
+    res.status(500).json({message: 'Something went wrong'})
+  } )
 });
 
 router.post('/', (req, res) => {
@@ -42,3 +57,4 @@ router.post('/:id/posts', (req, res) => {
 });
 
 // do not forget to export the router
+module.exports = router;
